e2dd82383f43efc78c6b0eb3089c884ed3499f3f
MrCronkite <vladshima99@gmail.com>
2023-04-05 23:00:55 +0300

    fix filles

--- iDeluxeApp/Aplication/SceneDelegate.swift
+++ iDeluxeApp/Aplication/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AuthSDK
 
 final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -19,7 +20,7 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = ViewController()
+        window?.rootViewController = AuthViewController()
         window?.makeKeyAndVisible()
     }
 }
